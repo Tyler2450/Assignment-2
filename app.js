e0c3b83cd02ada103e575a0f111f7c8b2a2efeb7
Tyler2450 <tfurtado0311@gmail.com>
2024-11-05 22:33:10 -0500

    Final Commit

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the router
 app.use('/', indexRouter);
 
 module.exports = app;
