4839aae34e3939e5fab4f6a80511b3ed0766669f
Tyler2450 <tfurtado0311@gmail.com>
2024-11-08 20:31:14 -0500

    Final Part 2

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const indexRouter = require('./routes/index');
 const app = express();
+const port = process.env.PORT || 3000;  // Render will set this
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
